6ce2fb189f5ed7f370dc63d4f0dcb80ebfd33a9d
dashtm4 <dashtm4@gmail.com>
2022-01-19 17:06:29 -0500

    feat: add DocuSign panel

--- src/containers/ExplorePage/Explore.tsx
+++ src/containers/ExplorePage/Explore.tsx
@@ -2,19 +2,36 @@ import styled from "styled-components";
 
 import ExperiencePanel from "../../components/ExperiencePanel";
 import DefiSwap from "../../assets/images/defi-swap.png";
+import DocuSign from "../../assets/images/docu-sign.png";
 
-const StyledSectionTitle = styled.div``;
+const StyledSectionTitle = styled.div.attrs({
+  className: "",
+})``;
+
+const StyledSection = styled.div.attrs({
+  className: "px-",
+})`
+  padding: 0 18px;
+`;
 
 function Explore() {
   return (
     <div>
-      <StyledSectionTitle>Recent Experiences</StyledSectionTitle>
-      <ExperiencePanel
-        logo={DefiSwap}
-        title="DeFi Swap"
-        description="Swap your digital assets"
-        users={200}
-      />
+      <StyledSection className="px-">
+        <StyledSectionTitle className="">Recent Experiences</StyledSectionTitle>
+        <ExperiencePanel
+          logo={DefiSwap}
+          title="DeFi Swap"
+          description="Swap your digital assets"
+          users={200}
+        />
+        <ExperiencePanel
+          logo={DocuSign}
+          title="Docu Sign"
+          description="sign smart contracts seamlessly"
+          users={1000}
+        />
+      </StyledSection>
     </div>
   );
 }
